ecc018ddbf00ed64392d16c20f3b4bf885448280
Krystian <krystian.konecki@yahoo.com>
2020-02-13 14:55:34 +0100

    Updates

---
+++ precache-manifest.772c86fe86cb40b80f9e4cfbb3a4f325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c4835fdbb27025df3d78154560f377",
+    "url": "/TheMovie/index.html"
+  },
+  {
+    "revision": "b4112e6778cf9251902a",
+    "url": "/TheMovie/static/css/main.03874dcc.chunk.css"
+  },
+  {
+    "revision": "16ce615625ae3cad1663",
+    "url": "/TheMovie/static/js/2.9206302b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/TheMovie/static/js/2.9206302b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4112e6778cf9251902a",
+    "url": "/TheMovie/static/js/main.2fa672af.chunk.js"
+  },
+  {
+    "revision": "e8465942a144108a9ab3",
+    "url": "/TheMovie/static/js/runtime-main.2e280fcb.js"
+  }
+]);
