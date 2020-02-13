42b75c72d467ad9660e1a4e8204fab41fdd49f15
Krystian <krystian.konecki@yahoo.com>
2020-02-13 16:04:19 +0100

    Adding ErrorPage page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import "../sass/errorPage.sass"
 
 const ErrorPage = () => {
     return (  
-        <h2>Error Page</h2>
+        <div className="main__errorPage errorPage">
+            <h2 className="errorPage__info">The page with the given URL does not exist</h2>
+        </div>
     );
 }
  
